5ac0c98fad58c8db9f4368e8261ebf63955e6dc6
Jose Rodriguez <cyb3rpandah@gmail.com>
2022-09-24 22:20:20 -0400

    Initial review of OSSEM and Sigma fields
    Findings:
    - Hashes: this field contains multiple hashes. OSSEM Hash entity has fields per hash type.
    - OriginalFileName: this field is currently not part of the Process entity
    - SourceImage: OSSEM handles this field as ProcessFilePath. We do not add the source prefix. Image and SourceImage both are mapped to ProcessFilePath
    - TargetImage: OSSEM uses TargetProcessFilePath. The Process entity extends the Target entity but not Source (Source is currently used only for network connections context). Should we continue using a target entity or define prefixes? (Also consider Actor, Acting, Src, and Dst prefixes)
    - There are ProcessParent* fields that are available from an OSSEM perspective, but I haven't seen its use in Sigma so far. This review is still a work in progress.
    - ComputerName: The entity that might help us to map this field is Device. However, this entity does not have a name field. We have the Hostname field though. Should we use Hostname to map this field? Should we add the name field?
    - Event data Vs System data: When standardizing Windows events, we standardize only fields within EventData. We do not standardize fields within System such as Provider Name or Channel. Since we do not standardize System fields, they are not part of OSSEM entities. How should we handle this types of fields?

--- sigma/pipelines/ossem/ossem.py
+++ sigma/pipelines/ossem/ossem.py
@@ -11,10 +11,47 @@ def ossem_to_sigma() -> ProcessingPipeline:      # Processing pipelines should b
             ProcessingItem(     # Field mappings
                 identifier="ossem_field_mapping",
                 transformation=FieldMappingTransformation({
-                    "process_command_line": "CommandLine",
-                    "process_file_path": "Image",
-                    "process_parent_command_line": "ParentCommandLine",
-                    "process_parent_file_path": "ParentImage",
+                    # Process Entity 
+                    #### / process_creation
+                    "ProcessCommandLine": "CommandLine",
+                    "ProcessFilePath": "Image",
+                    "ProcessIntegrityLevel":"IntegrityLevel",
+                    "ProcessFileProduct":"Product",
+                    "ProcessFileDescription":"Description",
+                    "ProcessFileCompany":"Company",
+                    "ProcessFileDirectory":"CurrentDirectory",
+                    "ProcessHashSha1":"sha1",
+                    "ProcessHashImphash":"Imphash",
+                    "ProcessHashImphash":"Hashes", # check https://github.com/SigmaHQ/sigma/blob/master/rules/windows/process_creation/proc_creation_win_bypass_squiblytwo.yml
+                    "":"OriginalFileName", #----> Field is not part of Process Entity, check Sysmon 1
+                    #### / process_access
+                    "ProcessCallTrace":"CallTrace",
+                    "ProcessGrantedAccess":"GrantedAccess",
+                    "ProcessFilePath":"SourceImage", # Image and SourceImage would have the same OSSEM mapping
+                    "TargetProcessFilePath":"TargetImage",
+                    #### Process Entity (ProcessParent) / process_creation
+                    "ProcessParentCommandLine": "ParentCommandLine",
+                    "ProcessParentFilePath": "ParentImage",
+                    "ProcessParentIntegrityLevel":"",
+                    "ProcessParentProduct":"",
+                    "ProcessParentFileDescription":"",
+                    "ProcessParentFileCompany":"",
+                    "ProcessParentFileDirectory":"",
+                    "ProcessParentHashSha1":"",
+                    "ProcessParentHashImphash":"",
+                    #### Process Entity (ProcessParent) / process_access
+                    "ProcessParentCallTrace": "",
+                    "ProcessParentGrantedAccess": "",
+                    # User Entity
+                    "UserName":"User",
+                    # Computer (Device from OSSEM??)
+                    "":"ComputerName", # example event 4776, uses Hostname. Device entity has Hostname field.
+                    # Logon Entity
+                    "LogonGuid":"LogonGuid",
+                    # Registry Entity
+                    "RegistryValueData":"Details", # I need to validate if Details comes from registry events https://github.com/SigmaHQ/sigma/blob/master/rules/windows/process_creation/proc_creation_win_cmstp_execution_by_creation.yml
+                    # Not Entity related fields
+                    "":"Provider_Name" # Currently, we only map fields from event_data no from the system_data https://github.com/SigmaHQ/sigma/blob/master/rules/windows/process_access/proc_access_win_direct_syscall_ntopenprocess.yml
                 })
             )
         ],
